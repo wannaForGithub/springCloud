c89138189589428e3431f40465d5da1ea1bc9adf
wangdh <wangdh@cc.com>
2021-03-25 09:11:12 +0800

    提交pom修改

--- product-service/src/main/java/com/example/productserver/ProductApplication.java
+++ product-service/src/main/java/com/example/productserver/ProductApplication.java
@@ -2,12 +2,10 @@ package com.example.productserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
+//@EnableEurekaClient
+//@EnableFeignClients
 public class ProductApplication {
 
     public static void main(String[] args) {
